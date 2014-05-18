cd7578d9cdf3c6d5f7f051f664a9641331a03ce7
Evan Jones <ej@evanjones.ca>
2014-05-18 16:49:37 -0400

    browser_duplicates: Test for duplicate names in JavaScript.

---
+++ browser_duplicates/duplicates.js
@@ -0,0 +1,10 @@
+function duplicateFunction() {
+  console.log('file function');
+}
+
+var duplicateVar = 'file var';
+duplicateNoVar = 'file no var';
+
+var duplicateVarFunction = function() {
+  console.log('file function duplicateVarFunction');
+};
